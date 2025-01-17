b3d8ec8be0caf68b9f785b32590ed49b64ed99dc
Aman Jangra <aman.jangra@proxmed.com.au>
2025-01-17 16:31:37 +0530

    Context API for Auth

--- frontEnd/my-app/src/App.js
+++ frontEnd/my-app/src/App.js
@@ -2,16 +2,21 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./components/Auth/Login";
 import Signup from "./components/Auth/SignUp";
-import Dashboard from "./pages/DashBoard"
+import Dashboard from "./pages/DashBoard";
+import AuthProvider from "./context/AuthContext"; // Adjust the import path as necessary
+ 
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Login />} /> 
-        <Route path="/dashboard" element={<Dashboard />} /> 
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/signup" element={<Signup />} />
+          
+        </Routes>
+      </AuthProvider>
     </Router>
   );
 }
